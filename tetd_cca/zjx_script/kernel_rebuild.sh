00f3f1c24961f1b65bb2b47adc549b6dd3c6f0f4
recluseZhan <1229233989@qq.com>
2025-01-14 14:34:41 +0800

    no1

--- tetd_cca/zjx_script/kernel_rebuild.sh
+++ tetd_cca/zjx_script/kernel_rebuild.sh
@@ -2,12 +2,12 @@
 
 cd ../linux
 
-export CROSS_COMPILE=/home/ubuntu/rme-stack/buildroot/output/host/bin/aarch64-linux-
+export CROSS_COMPILE=/home/zjx/rme-stack/buildroot/output/host/bin/aarch64-linux-
 
 make ARCH=arm64 prepare
 make ARCH=arm64 -j$(nproc)
 make ARCH=arm64 modules -j$(nproc)
-sudo make ARCH=arm64 CROSS_COMPILE=$CROSS_COMPILE INSTALL_MOD_PATH=/home/ubuntu/rme-stack/linux modules_install
+sudo make ARCH=arm64 CROSS_COMPILE=$CROSS_COMPILE INSTALL_MOD_PATH=/home/zjx/rme-stack/linux modules_install
 
 cd ..
 ./build-scripts/aemfvp-a-rme/build-test-buildroot.sh -p aemfvp-a-rme package
---
+++ tetd_x86/zjx_script/re_kvm.sh
@@ -0,0 +1,9 @@
+cd ~/linux-intel-6.8.0/arch/x86/kvm
+sudo make -C /lib/modules/`uname -r`/build M=`pwd` clean
+sudo make -C /lib/modules/`uname -r`/build M=`pwd` modules
+sudo modprobe -r kvm_intel
+sudo modprobe -r kvm
+modprobe irqbypass
+sudo insmod kvm.ko
+sudo insmod kvm-intel.ko
+
